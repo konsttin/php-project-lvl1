637dc950a95f48698b61101113b81047f461293a
Konstantin Glebov <5449277@gmail.com>
2021-09-28 13:32:43 +0300

    edit progression games

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -4,23 +4,31 @@ namespace Brain\Games\Progression;
 
 use function Brain\Engine\engine;
 
+function generateProgression():array
+{
+    $startProgression = random_int(1, 50);
+    $stepProgression = random_int(1, 10);
+    $arrayProgression = [];
+    for ($x = $startProgression, $loopsMax = count($arrayProgression); $loopsMax < 10; $x++) {
+        $x += $stepProgression;
+        $arrayProgression[] = $x;
+    }
+    return $arrayProgression;
+}
+
 function progression()
 {
     $task = 'What number is missing in the progression?';
     $gameData = [];
     for ($i = 0; $i <= 2; $i++) {
-        $randomNumber1 = random_int(1, 50);
-        $randomNumber2 = random_int(1, 10);
-        $array[] = $randomNumber1;
-        for ($index = 1; $index < 10; $index++) {
-            $array[$index] = $array[$index - 1] + $randomNumber2;
-        }
-        $randomNumber3 = random_int(0, 9);
-        $correctAnswer = $array[$randomNumber3];
-        $array[$randomNumber3] = '..';
+        $progression = generateProgression();
+        $randomNumberQuestion = random_int(0, 9);
+        $correctAnswer = $progression[$randomNumberQuestion];
+        $progression[$randomNumberQuestion] = '..';
 
-        $question = implode(' ', $array);
+        $question = implode(' ', $progression);
         $gameData[] = ['question' => $question, 'correctAnswer' => (string)$correctAnswer];
+
     }
 
     engine($task, $gameData);
